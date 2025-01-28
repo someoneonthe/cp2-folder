83dd530bd4f4ad7b4f3c1003bdc5b25e28039044
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2025-01-28 21:10:01 +0000

    3:09 1/28/2025

--- prog52a.py
+++ prog52a.py
@@ -1,6 +1,7 @@
 length = int(input("Enter leanth:"))
-width = int(input("Enter width"))
+width = int(input("Enter width:"))
 area = length * width
 perim = 2 * length + 2 * width
 print("Area:", area)
 print("perimiter:", perim)
+print("thats all folks")
