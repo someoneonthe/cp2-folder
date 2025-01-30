c1d0892c6fa2cdc37a805bd16ce0056471bf7c92
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2025-01-30 20:56:34 +0000

    E

---
+++ prog54b.py
@@ -0,0 +1,6 @@
+w = int(input("enter your first number:"))
+x = int(input("enter your second number:"))
+y = int(input("enter your third number:"))
+z = int(input("enter your forth number:"))
+a = w + x + y + z
+print ("= ", a)
