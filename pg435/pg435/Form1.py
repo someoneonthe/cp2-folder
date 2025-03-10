27b25ee9cdc09673eed4a6d771a12da9317503d0
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2025-03-10 14:49:16 -0500

    langdat

--- pg435/pg435/Form1.py
+++ pg435/pg435/Form1.py
@@ -11,80 +11,45 @@ class Form1(Form):
 		self.myparent = parent
 	
 	def InitializeComponent(self):
-		self._radioButton1 = System.Windows.Forms.RadioButton()
-		self._radioButton2 = System.Windows.Forms.RadioButton()
-		self._radioButton3 = System.Windows.Forms.RadioButton()
 		self._textBox1 = System.Windows.Forms.TextBox()
 		self._textBox2 = System.Windows.Forms.TextBox()
 		self._textBox3 = System.Windows.Forms.TextBox()
-		self._textBox4 = System.Windows.Forms.TextBox()
 		self._button1 = System.Windows.Forms.Button()
 		self._button2 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
 		self.SuspendLayout()
-		# 
-		# radioButton1
-		# 
-		self._radioButton1.Location = System.Drawing.Point(12, 12)
-		self._radioButton1.Name = "radioButton1"
-		self._radioButton1.Size = System.Drawing.Size(104, 24)
-		self._radioButton1.TabIndex = 0
-		self._radioButton1.TabStop = True
-		self._radioButton1.Text = "radioButton1"
-		self._radioButton1.UseVisualStyleBackColor = True
-		# 
-		# radioButton2
-		# 
-		self._radioButton2.Location = System.Drawing.Point(12, 52)
-		self._radioButton2.Name = "radioButton2"
-		self._radioButton2.Size = System.Drawing.Size(104, 24)
-		self._radioButton2.TabIndex = 1
-		self._radioButton2.TabStop = True
-		self._radioButton2.Text = "radioButton2"
-		self._radioButton2.UseVisualStyleBackColor = True
-		# 
-		# radioButton3
-		# 
-		self._radioButton3.Location = System.Drawing.Point(12, 94)
-		self._radioButton3.Name = "radioButton3"
-		self._radioButton3.Size = System.Drawing.Size(104, 24)
-		self._radioButton3.TabIndex = 2
-		self._radioButton3.TabStop = True
-		self._radioButton3.Text = "radioButton3"
-		self._radioButton3.UseVisualStyleBackColor = True
 		# 
 		# textBox1
 		# 
-		self._textBox1.Location = System.Drawing.Point(154, 12)
+		self._textBox1.Location = System.Drawing.Point(94, 6)
 		self._textBox1.Name = "textBox1"
-		self._textBox1.Size = System.Drawing.Size(100, 20)
+		self._textBox1.Size = System.Drawing.Size(100, 31)
 		self._textBox1.TabIndex = 3
 		# 
 		# textBox2
 		# 
-		self._textBox2.Location = System.Drawing.Point(154, 52)
+		self._textBox2.Location = System.Drawing.Point(94, 43)
 		self._textBox2.Name = "textBox2"
-		self._textBox2.Size = System.Drawing.Size(100, 20)
+		self._textBox2.Size = System.Drawing.Size(100, 31)
 		self._textBox2.TabIndex = 4
 		# 
 		# textBox3
 		# 
-		self._textBox3.Location = System.Drawing.Point(154, 78)
+		self._textBox3.Location = System.Drawing.Point(94, 80)
 		self._textBox3.Name = "textBox3"
-		self._textBox3.Size = System.Drawing.Size(100, 20)
+		self._textBox3.Size = System.Drawing.Size(100, 31)
 		self._textBox3.TabIndex = 5
-		# 
-		# textBox4
-		# 
-		self._textBox4.Location = System.Drawing.Point(154, 104)
-		self._textBox4.Name = "textBox4"
-		self._textBox4.Size = System.Drawing.Size(100, 20)
-		self._textBox4.TabIndex = 6
+		self._textBox3.Text = "0.05"
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(30, 141)
+		self._button1.Location = System.Drawing.Point(228, 25)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.Size = System.Drawing.Size(94, 40)
 		self._button1.TabIndex = 7
 		self._button1.Text = "Solve"
 		self._button1.UseVisualStyleBackColor = True
@@ -92,26 +57,71 @@ class Form1(Form):
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(167, 141)
+		self._button2.Location = System.Drawing.Point(228, 71)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(75, 23)
+		self._button2.Size = System.Drawing.Size(94, 40)
 		self._button2.TabIndex = 8
-		self._button2.Text = "Quit"
+		self._button2.Text = "Back"
 		self._button2.UseVisualStyleBackColor = True
 		self._button2.Click += self.Button2Click
 		# 
+		# label1
+		# 
+		self._label1.Location = System.Drawing.Point(12, 9)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(76, 23)
+		self._label1.TabIndex = 9
+		self._label1.Text = "num of tickets"
+		# 
+		# label2
+		# 
+		self._label2.Location = System.Drawing.Point(12, 83)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(52, 23)
+		self._label2.TabIndex = 10
+		self._label2.Text = "tax"
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.SystemColors.ControlLightLight
+		self._label3.Location = System.Drawing.Point(81, 151)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(100, 23)
+		self._label3.TabIndex = 11
+		self._label3.Text = "label3"
+		self._label3.Click += self.Label3Click
+		# 
+		# label4
+		# 
+		self._label4.Location = System.Drawing.Point(12, 151)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(63, 23)
+		self._label4.TabIndex = 12
+		self._label4.Text = "total"
+		# 
+		# label5
+		# 
+		self._label5.Location = System.Drawing.Point(12, 43)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(76, 23)
+		self._label5.TabIndex = 13
+		self._label5.Text = "price"
+		# 
 		# Form1
 		# 
-		self.ClientSize = System.Drawing.Size(286, 201)
+		self.BackColor = System.Drawing.SystemColors.ControlLight
+		self.ClientSize = System.Drawing.Size(334, 192)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
-		self.Controls.Add(self._textBox4)
 		self.Controls.Add(self._textBox3)
 		self.Controls.Add(self._textBox2)
 		self.Controls.Add(self._textBox1)
-		self.Controls.Add(self._radioButton3)
-		self.Controls.Add(self._radioButton2)
-		self.Controls.Add(self._radioButton1)
+		self.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self.Name = "Form1"
 		self.Text = "Form1"
 		self.Load += self.Form1FormClosing
@@ -119,7 +129,12 @@ class Form1(Form):
 		self.PerformLayout()
 
 	def Button1Click(self, sender, e):
-		pass
+		num = int(self._textBox1.Text)
+		price = float(self._textBox2.Text)
+		tax = float(self._textBox3.Text)
+		ttax = tax + 1
+		total = (num * price) * ttax
+		self._label3.Text = str(total)
 
 	def Form1FormClosing(self, sender, e):
 		self.myparent.Show()
@@ -127,3 +142,6 @@ class Form1(Form):
 	def Button2Click(self, sender, e):
 		self.myparent.Show()
 		self.Close()
+
+	def Label3Click(self, sender, e):
+		self._label3.Text = "BOO"
--- pg435/pg435/MainForm.py
+++ pg435/pg435/MainForm.py
@@ -16,28 +16,29 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
-		self._button1.Location = System.Drawing.Point(187, 55)
+		self._button1.Location = System.Drawing.Point(12, 12)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.Size = System.Drawing.Size(102, 82)
 		self._button1.TabIndex = 0
-		self._button1.Text = "General"
+		self._button1.Text = "General Tickets"
 		self._button1.UseVisualStyleBackColor = True
 		self._button1.Click += self.Button1Click
 		# 
 		# button2
 		# 
-		self._button2.Location = System.Drawing.Point(187, 84)
+		self._button2.Location = System.Drawing.Point(120, 12)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(75, 23)
+		self._button2.Size = System.Drawing.Size(95, 82)
 		self._button2.TabIndex = 1
-		self._button2.Text = "Student"
+		self._button2.Text = "Student Tickets"
 		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
-		self._button3.Location = System.Drawing.Point(187, 156)
+		self._button3.Location = System.Drawing.Point(12, 100)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(75, 23)
+		self._button3.Size = System.Drawing.Size(203, 35)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Quit"
 		self._button3.UseVisualStyleBackColor = True
@@ -45,10 +46,11 @@ class MainForm(Form):
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(284, 261)
+		self.ClientSize = System.Drawing.Size(226, 149)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
+		self.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self.Name = "MainForm"
 		self.Text = "pg435"
 		self.ResumeLayout(False)
@@ -62,3 +64,9 @@ class MainForm(Form):
 
 	def Button3Click(self, sender, e):
 		Application.Exit()
+
+	def Button2Click(self, sender, e):
+		from Form2 import *
+		form2 = Form2(self)
+		form2.Show()
+		self.Hide()
